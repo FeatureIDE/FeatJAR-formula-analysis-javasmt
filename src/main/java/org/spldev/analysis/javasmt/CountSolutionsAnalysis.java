2f120bcdbde67455082f4d6669c2c687bcf58df1
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:01 +0200

    Updated license headers

--- src/main/java/org/spldev/analysis/javasmt/CountSolutionsAnalysis.java
+++ src/main/java/org/spldev/analysis/javasmt/CountSolutionsAnalysis.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Formula-Analysis-JavaSMT Lib - Library to analyze propositional formulas with JavaSMT.
- * Copyright (C) 2021  Sebastian Krieter
+ * formula-analysis-javasmt - Analysis of first-order formulas using JavaSMT
+ * Copyright (C) 2021 Sebastian Krieter
  * 
- * This file is part of Formula-Analysis-JavaSMT Lib.
+ * This file is part of formula-analysis-javasmt.
  * 
- * Formula-Analysis-JavaSMT Lib is free software: you can redistribute it and/or modify it
+ * formula-analysis-javasmt is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Formula-Analysis-JavaSMT Lib is distributed in the hope that it will be useful,
+ * formula-analysis-javasmt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Formula-Analysis-JavaSMT Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with formula-analysis-javasmt. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/formula-analysis-javasmt> for further information.
+ * See <https://github.com/FeatJAR/formula-analysis-javasmt> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.analysis.javasmt;
