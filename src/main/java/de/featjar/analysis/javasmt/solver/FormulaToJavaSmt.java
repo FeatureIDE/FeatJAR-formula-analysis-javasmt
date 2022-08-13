c95159393e732fb5860050b99c548a1c7ef8ce9f
Elias Kuiter <info@elias-kuiter.de>
2022-08-13 11:03:37 +0200

    License

--- src/main/java/de/featjar/analysis/javasmt/solver/FormulaToJavaSmt.java
+++ src/main/java/de/featjar/analysis/javasmt/solver/FormulaToJavaSmt.java
@@ -1,24 +1,22 @@
-/* -----------------------------------------------------------------------------
- * formula-analysis-javasmt - Analysis of first-order formulas using JavaSMT
- * Copyright (C) 2021 Sebastian Krieter
- * 
+/*
+ * Copyright (C) 2022 Sebastian Krieter
+ *
  * This file is part of formula-analysis-javasmt.
- * 
+ *
  * formula-analysis-javasmt is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
- * 
+ *
  * formula-analysis-javasmt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with formula-analysis-javasmt. If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  * See <https://github.com/FeatJAR/formula-analysis-javasmt> for further information.
- * -----------------------------------------------------------------------------
  */
 package de.featjar.analysis.javasmt.solver;
 
